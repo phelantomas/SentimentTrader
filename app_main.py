61ea2c319774cd6891e7a53eb53e28975ac854f3
phelantomas <31985716+phelantomas@users.noreply.github.com>
2018-01-23 20:42:55 +0000

    Added updating features

--- app_main.py
+++ app_main.py
@@ -3,14 +3,12 @@ Author: Tomas Phelan
 License Employed: GNU General Public License v3.0
 Brief:
 '''
-from apscheduler.schedulers.blocking import BlockingScheduler
+
 import collect_tweets, collect_prices, process_tweets, json
 import pandas as pd
 import os.path
 import predict
-import log_creater as lc
 import datetime
-
 import matplotlib
 
 matplotlib.use("TkAgg")
@@ -22,42 +20,27 @@ from matplotlib import style
 from Tkinter import *
 import Tkinter as tk
 import ttk
-
 import pandas as pd
-
 import threading
-
 
 LARGE_FONT = ("Verdana", 12)
 style.use("ggplot")
 
-f = Figure(figsize=(5, 5), dpi=100)
-a = f.add_subplot(111)
+btcF = Figure(figsize=(5, 5), dpi=100)
+btcA = btcF.add_subplot(111)
 
 formatted_btc_tweets = []
 formatted_ltc_tweets = []
 formatted_eth_tweets = []
-predict_change = 0
-
-twitter_logger = lc.setup_logger('first_logger', 'twitter_logfile.log')
-
-sched = BlockingScheduler()
+btc_predict_change = 0
+ltc_predict_change = 0
+eth_predict_change = 0
+btc_predict_change_model = None
+ltc_predict_change_model = None
+eth_predict_change_model = None
 num_of_passes = 0
-
-def animateBtcPrice(file, Num):
-    pullData = pd.read_csv(file, error_bad_lines=False)
-
-    pullData = pullData.tail(n=Num)
-    xList = []
-    yList = []
-    index1 = 0
-    for index, row in pullData.iterrows():
-        xList.append(int(index1))
-        yList.append(int(row['Price']))
-        index1 = index1 + 1
-
-    a.clear()
-    a.plot(xList, yList)
+btc_var_pred_text = None
+btcTree = None
 
 def analyse_data(formatted_tweets, filename, exchange, coin):
     global predict_change
@@ -76,8 +59,6 @@ def analyse_data(formatted_tweets, filename, exchange, coin):
             btcTweetsInHour.append(tweet)
 
     print("Number of unique tweets in an hour for " + coin + " is " + str(len(btcTweetsInHour)))
-
-
 
     file_exists = os.path.isfile(filename)
 
@@ -106,15 +87,17 @@ def analyse_data(formatted_tweets, filename, exchange, coin):
         print("The sentiment of the last 60 minutes for " + coin + " is : " + str(
         cryptoFeature['Sentiment'][0]) + " - The predicted change in price is : " + predict_change)
 
-
-#@sched.scheduled_job('interval', minutes=1)
-def timed_job():
-    threading.Timer(60.0, timed_job).start()  # called every minute
+def collect_data():
+    threading.Timer(60.0, collect_data).start()  # called every minute
 
     global formatted_btc_tweets
     global formatted_ltc_tweets
     global formatted_eth_tweets
     global num_of_passes
+    global btc_predict_change
+    global btc_predict_change_model
+    global btc_var_pred_text
+    global btcTree
 
     num_of_passes = num_of_passes + 1
 
@@ -125,6 +108,29 @@ def timed_job():
     btcTweets = process_tweets.process_tweets_from_main(btcTweets)
 
     formatted_btc_tweets.extend(btcTweets)
+
+    if btcTree:
+        btcTree.delete(*btcTree.get_children()) #Clears tree to prevent duplicates
+        for tweet in formatted_btc_tweets:
+            btcTree.insert("", 'end', text="L1", values=(str(tweet['created_at']), str(tweet['formatted_text']), str(tweet['sentiment']['compound'])))
+
+    if btc_predict_change_model is None:
+        btc_predict_change_model = predict.generate_linear_prediction_model_init("btcFeature.csv")
+
+    #Generate new prediction every minute
+
+    # average compound
+    average_compound = float(sum(d['sentiment']['compound'] for d in formatted_btc_tweets)) / len(
+        formatted_btc_tweets)
+
+    regFeature = {'Sentiment': [average_compound]}
+
+    dfFeature = pd.DataFrame(regFeature)
+
+    btc_predict_change = str(btc_predict_change_model.predict(dfFeature))
+
+    if btc_var_pred_text:
+        btc_var_pred_text.set("Predicted change in price is " + str(btc_predict_change))
 
     #litecoin
     ltcTweets = collect_tweets.collect_tweets('litecoin')
@@ -148,13 +154,40 @@ def timed_job():
         analyse_data(formatted_eth_tweets, "ethFeature.csv", "eth-usd", "Etherium")
         formatted_eth_tweets = []
 
-class SeaofBTCapp(tk.Tk):
+def animateBtcPrice(file, Num):
+    btcA.clear()
+    pullData = pd.read_csv(file, error_bad_lines=False)
+
+    pullData = pullData.tail(n=Num)
+    xList = []
+    yList = []
+    for index, row in pullData.iterrows():
+        hour = datetime.datetime.fromtimestamp(int(row['TimeStamp'])).hour
+        xList.append(int(hour))
+        yList.append(int(row['Price']))
+
+    btcA.set_title('Bitcoin Price Per Hour')
+    btcA.set_ylabel('Price ($)')
+    btcA.set_xlabel('time (h)')
+    btcA.plot(xList, yList)
+
+def quitHandler():
+    app.destroy()
+    print ("Goodbye")
+    sys.exit(0)
+
+class SentimentTradingApp(tk.Tk):
 
     def __init__(self, *args, **kwargs):
+        global btc_var_pred_text
+        global btcTree
+
         tk.Tk.__init__(self, *args, **kwargs)
 
         tk.Tk.iconbitmap(self, ) #default="clienticon.ico"
-        tk.Tk.wm_title(self, "Sea of BTC client")
+        tk.Tk.wm_title(self, "Automated Sentiment Trading")
+
+        btc_var_pred_text = StringVar()
 
         container = tk.Frame(self)
         container.pack(side="top", fill="both", expand=True)
@@ -163,7 +196,7 @@ class SeaofBTCapp(tk.Tk):
 
         self.frames = {}
 
-        for F in (StartPage, PageOne, PageTwo, PageThree, PageBtcTweets): #, MultiListbox
+        for F in (StartPage, PageMainBtc, PageMainLtc, PageBtcGraph, PageBtcTweets):
             frame = F(container, self)
 
             self.frames[F] = frame
@@ -176,8 +209,6 @@ class SeaofBTCapp(tk.Tk):
         frame = self.frames[cont]
         frame.tkraise()
 
-
-
 class StartPage(tk.Frame):
 
     def __init__(self, parent, controller):
@@ -187,28 +218,29 @@ class StartPage(tk.Frame):
         label.pack(pady=10, padx=10)
 
         button = ttk.Button(self, text="Bitcoin",
-                            command=lambda: controller.show_frame(PageOne))
+                            command=lambda: controller.show_frame(PageMainBtc))
         button.pack(pady=10, padx=10, anchor='c')
 
         button2 = ttk.Button(self, text="Litecoin",
-                             command=lambda: controller.show_frame(PageTwo))
+                             command=lambda: controller.show_frame(PageMainLtc))
         button2.pack(pady=10, padx=10, anchor='c')
 
-        button3 = ttk.Button(self, text="Graph Page",
-                             command=lambda: controller.show_frame(PageThree))
+        button3 = ttk.Button(self, text="Shut Down", command=quitHandler)
         button3.pack(pady=10, padx=10, anchor='c')
 
-
-class PageOne(tk.Frame):
+class PageMainBtc(tk.Frame):
 
     def __init__(self, parent, controller):
-        global predict_change
+        global btc_var_pred_text
+
+        if len(btc_var_pred_text.get()) is 0:
+            btc_var_pred_text.set("Waiting for data")
 
         tk.Frame.__init__(self, parent)
         label = tk.Label(self, text="Bitcoin", font=LARGE_FONT)
         label.pack(pady=10, padx=10)
 
-        label2 = tk.Label(self, text="Predicted change in price is " + str(predict_change), font=LARGE_FONT)
+        label2 = tk.Label(self, textvariable=btc_var_pred_text, font=LARGE_FONT)
         label2.pack(pady=10, padx=10)
 
         button1 = ttk.Button(self, text="Back to Home",
@@ -219,11 +251,10 @@ class PageOne(tk.Frame):
                              command=lambda: controller.show_frame(PageBtcTweets))
         button2.pack(pady=10, padx=10)
         button3 = ttk.Button(self, text="Bitcoin Price",
-                             command=lambda: controller.show_frame(PageThree))
+                             command=lambda: controller.show_frame(PageBtcGraph))
         button3.pack(pady=10, padx=10)
 
-
-class PageTwo(tk.Frame):
+class PageMainLtc(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
@@ -234,23 +265,18 @@ class PageTwo(tk.Frame):
                              command=lambda: controller.show_frame(StartPage))
         button1.pack()
 
-        button2 = ttk.Button(self, text="Page One",
-                             command=lambda: controller.show_frame(PageOne))
-        button2.pack()
-
-
-class PageThree(tk.Frame):
+class PageBtcGraph(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text="Graph Page!", font=LARGE_FONT)
+        label = tk.Label(self, text="Bitcoin Graph", font=LARGE_FONT)
         label.pack(pady=10, padx=10)
 
-        button1 = ttk.Button(self, text="Back to Home",
-                             command=lambda: controller.show_frame(StartPage))
+        button1 = ttk.Button(self, text="Back to Bitcoin Home",
+                             command=lambda: controller.show_frame(PageMainBtc))
         button1.pack()
 
-        canvas = FigureCanvasTkAgg(f, self)
+        canvas = FigureCanvasTkAgg(btcF, self)
         canvas.show()
         canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
 
@@ -260,42 +286,39 @@ class PageThree(tk.Frame):
 
 class PageBtcTweets(tk.Frame):
 
+
     def __init__(self, parent, controller):
         global formatted_btc_tweets
+        global btcTree
+
         tk.Frame.__init__(self, parent)
         label = tk.Label(self, text="BTC Tweets", font=LARGE_FONT)
         label.pack(pady=10, padx=10)
 
         button1 = ttk.Button(self, text="Back to Bitcoin Home",
-                             command=lambda: controller.show_frame(PageOne))
+                             command=lambda: controller.show_frame(PageMainBtc))
         button1.pack()
 
-        tree = ttk.Treeview(self, selectmode='browse')
-        tree.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
+        btcTree = ttk.Treeview(self, selectmode='browse')
+        btcTree.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
 
-        vsb = ttk.Scrollbar(tree, orient="vertical", command=tree.yview)
+        vsb = ttk.Scrollbar(btcTree, orient="vertical", command=btcTree.yview)
         vsb.pack(side='right', fill='y')
 
-        tree.configure(yscrollcommand=vsb.set)
+        btcTree.configure(yscrollcommand=vsb.set)
 
-        tree["columns"] = ("1", "2", "3")
-        tree['show'] = 'headings'
-        tree.column("1", width=150, anchor='c', stretch=False)
-        tree.column("2", width=900, anchor='w')
-        tree.column("3", width=100, anchor='c', stretch=False)
-        tree.heading("1", text="TimeStamp")
-        tree.heading("2", text="Tweet")
-        tree.heading("3", text="Sentiment")
-
-        for tweet in formatted_btc_tweets:
-            print(str(tweet['formatted_text']))
-            tree.insert("", 'end', text="L1", values=(str(tweet['created_at']), str(tweet['formatted_text']), str(tweet['sentiment']['compound'])))
-
+        btcTree["columns"] = ("1", "2", "3")
+        btcTree['show'] = 'headings'
+        btcTree.column("1", width=150, anchor='c', stretch=False)
+        btcTree.column("2", width=900, anchor='w')
+        btcTree.column("3", width=100, anchor='c', stretch=False)
+        btcTree.heading("1", text="TimeStamp")
+        btcTree.heading("2", text="Tweet")
+        btcTree.heading("3", text="Sentiment")
 
 if __name__ == '__main__':
-    timed_job()
-    #sched.start()
-    app = SeaofBTCapp()
+    app = SentimentTradingApp()
+    collect_data()
     app.geometry("1280x720")
-    ani = animation.FuncAnimation(f, animateBtcPrice("btcFeature.csv", 15), interval=1000)
+    ani = animation.FuncAnimation(btcF, animateBtcPrice("btcFeature.csv", 8), interval=1000)
     app.mainloop()
