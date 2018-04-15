e2f59588726e3e186d0e521e521e4caeae6a1664
phelantomas <31985716+phelantomas@users.noreply.github.com>
2018-04-15 16:12:47 +0100

    Update sentiment_config.py

--- sentiment_config.py
+++ sentiment_config.py
@@ -3,8 +3,8 @@
 #EXCHANGE is stock symbol or cyrpto symbol i.e GOOGL for stock and btc-usd for bitcoin
 #FEATURE_FILE is where the features get saved
 #PAST_PREDICTIONS_FILE is where past predictions are stored
-TYPE = "STOCK"
-NAME = "Bank of America"
-EXCHANGE = "BAC"
-FEATURE_FILE = "Features/bacFeature.csv"
-PAST_PREDICTIONS_FILE = "Past_Predictions/bacPastPredictions.csv"
+TYPE = "CRYPTO"
+NAME = "Bitcoin"
+EXCHANGE = "btc-usd"
+FEATURE_FILE = "Features/btcFeature.csv"
+PAST_PREDICTIONS_FILE = "Past_Predictions/btcPastPredictions.csv"
